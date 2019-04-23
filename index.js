f0998084ffaeff44b1ed0dd9633deff53d9b88f4
Muhammad Abdullah Munir <munirabdullahm@gmail.com>
2019-04-23 12:55:40 +0700

    Add FCM on add Voucher

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ const serviceAccount = require('./amazonk-firestore.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://k-72b41.firebaseio.com"
 });
 
 const db = admin.firestore();
+
+var opened_shelf = {};
 
 app.get('/scoreboard', function(req, res) {
   db.collection('scoreboards').get()
@@ -63,9 +66,19 @@ app.post('/add-score', function(req, res) {
 });
 
 app.post('/add-voucher', function(req, res) {
-  console.log(req.body);
   if (req.body.email != undefined) {
+    const topic = req.body.email.replace(/[@+]/, "").replace(".", "");
     const docRef = db.collection('vouchers').doc(req.body.email);
+
+    console.log(topic);
+
+    const message = {
+      notification: {
+        title: 'Got Voucher',
+        body: 'You get voucher from AmazonkGame'
+      },
+      topic: topic
+    };
 
     docRef.get().then(function(doc) {
       if (doc.exists) {
@@ -78,6 +91,14 @@ app.post('/add-voucher', function(req, res) {
             }),
             gamevoucher: true,
           }, );
+
+          admin.messaging().send(message)
+            .then((response) => {
+              console.log('Successfully sent message:', response);
+            })
+            .catch((error) => {
+              console.log('Error sending message:', error);
+            });
 
           res.status(200).send({
             status: 'OK',
@@ -109,10 +130,50 @@ app.post('/add-voucher', function(req, res) {
 });
 
 app.post('/open-shelf', function(req, res) {
-  console.log(req.body);
-  res.status(200).send({
-    status: 'OK',
-  });
+  if (req.body.barang != undefined
+    && req.body.email != undefined) {
+
+    if (opened_shelf[req.body.barang] == undefined)
+      opened_shelf[req.body.barang] = [];
+
+    opened_shelf[req.body.barang].push(req.body.email);
+
+    res.status(200).send({
+      status: 'OK',
+    });
+  } else {
+    res.status(200).send({
+      status: 'Wrong Parameter',
+    });
+  }
+});
+
+app.post('/close-shelf', function(req, res) {
+  if (req.body.barang != undefined
+    && req.body.count != undefined) {
+
+    email = opened_shelf[req.body.barang].shift();
+
+    const docRef = db.collection('cart').doc(email);
+    docRef.update({
+      'productList' : FieldValue.arrayUnion({
+        'idProduk' : req.body.barang,
+        'jumlah' : req.body.count,
+      }),
+    });
+
+    res.status(200).send({
+      status: 'OK',
+    });
+  } else {
+    res.status(200).send({
+      status: 'Wrong Parameter',
+    });
+  }
+});
+
+app.get('/opened-shelf', function(req, res) {
+  res.status(200).send(opened_shelf);
 });
 
 port = process.env.PORT || 3000;
